138ffaca764c2f6495d7aa78200e2df8b7f892e1
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-01-30 09:47:41 +0300

    filter for goals updated

--- resources/views/goals/list.blade.php
+++ resources/views/goals/list.blade.php
@@ -1,20 +1,35 @@
+<style>
+    .custom-dropdown-menu {
+        background-color: rgb(26, 32, 53); /* Matches the dark background of the table header */
+        border-color: #454d55; /* Slightly lighter border to match the theme */
+    }
+    .custom-dropdown-menu .dropdown-item {
+        color: #fff; /* White text to maintain readability */
+    }
+    .custom-dropdown-menu .dropdown-item:hover {
+        background-color: #495057; /* Slightly lighter hover color */
+    }
+</style>
+
 <table class="table table-bordered table-striped">
     <thead class="thead-dark">
         <tr>
             <th>#</th>
-            <th class="d-flex align-items-center">
+            <th class="d-flex align-items-center text-white position-relative">
                 <span class="me-2">Strategy Name</span>
                 <form method="GET" action="{{ route('goals.index') }}" id="filter-form">
-                    <div class="position-relative">
-                        <select name="strategy_id" class="form-select" onchange="document.getElementById('filter-form').submit();" style="padding-right: 30px;">
-                            <option value="">All Strategies</option>
+                    <div class="dropdown">
+                        <i class="fa fa-chevron-down text-white px-3" role="button" data-bs-toggle="dropdown" aria-expanded="false"></i>
+                        <ul class="dropdown-menu custom-dropdown-menu mt-4">
+                            <li><a class="dropdown-item" href="{{ route('goals.index') }}">All Strategies</a></li>
                             @foreach($strategies as $strategy)
-                                <option value="{{ $strategy->id }}" {{ request('strategy_id') == $strategy->id ? 'selected' : '' }}>
-                                    {{ $strategy->name }}
-                                </option>
+                                <li>
+                                    <a class="dropdown-item" href="{{ route('goals.index', ['strategy_id' => $strategy->id]) }}">
+                                        {{ $strategy->name }}
+                                    </a>
+                                </li>
                             @endforeach
-                        </select>
-                        <i class="fa fa-chevron-down position-absolute" style="right: 10px; top: 50%; transform: translateY(-50%); pointer-events: none;"></i>
+                        </ul>
                     </div>
                 </form>
             </th>
