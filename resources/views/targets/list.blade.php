73218d22181ca721ad67dca048b60773954fe674
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-01-31 12:59:43 -0800

    target filter fixed

--- resources/views/targets/list.blade.php
+++ resources/views/targets/list.blade.php
@@ -7,22 +7,29 @@
             <th class="d-flex align-items-center text-white">
                 <span class="me-2">Goal</span>
                 @if(isset($goals) && count($goals) > 0)
-                <div class="dropdown">
-                    <i class="fa fa-chevron-down text-white px-3" role="button" data-bs-toggle="dropdown" aria-expanded="false">  <i class="fa-solid fa-filter text-white"></i> </i>
-
-                    <ul class="dropdown-menu custom-dropdown-menu">
-                        <li><a class="dropdown-item" href="{{ route('goals.index') }}">Show All Options</a></li>
-                        @foreach($goals as $goal)
+                    <div class="dropdown">
+                        <i class="fa fa-chevron-down text-white px-3" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i class="fa-solid fa-filter text-white"></i>
+                        </i>
+            
+                        <ul class="dropdown-menu custom-dropdown-menu">
                             <li>
-                                <a class="dropdown-item" href="{{ route('targets.index', ['goal_id' => $goal->id]) }}">
-                                    {{ $goal->name }}
+                                <a class="dropdown-item {{ request('goal_id') ? '' : 'active' }}" href="{{ route('targets.index') }}">
+                                    Show All Options
                                 </a>
                             </li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
+                            @foreach($goals as $goal)
+                                <li>
+                                    <a class="dropdown-item {{ request('goal_id') == $goal->id ? 'active' : '' }}" href="{{ route('targets.index', ['goal_id' => $goal->id]) }}">
+                                        {{ $goal->name }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
             </th>
+            
             <th>Actions</th>
         </tr>
     </thead>
