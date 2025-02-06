6f28dda0559c9225608d1b78267a5ebf0c4dd1ac
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-02-06 22:11:08 +0300

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,8 +24,70 @@
                             </div>
                             <div class="col col-stats ms-3">
                                 <div class="numbers">
+<<<<<<< Updated upstream
                                     <p class="card-category">Tasks</p>
                                     <h4 class="card-title">{{ count($tasks) }}</h4>
+=======
+                                    <p class="card-category">All Tasks</p>
+                                    <h4 class="card-title">{{ count($tasks) }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-icon">
+                                <div class="icon-big text-center icon-primary bubble-shadow-small">
+                                    <i class="fas fa-users"></i>
+                                </div>
+                            </div>
+                            <div class="col col-stats ms-3">
+                                <div class="numbers">
+                                    <p class="card-category">Pending Tasks</p>
+                                    <h4 class="card-title">{{ $tasks }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-icon">
+                                <div class="icon-big text-center icon-primary bubble-shadow-small">
+                                    <i class="fas fa-users"></i>
+                                </div>
+                            </div>
+                            <div class="col col-stats ms-3">
+                                <div class="numbers">
+                                    <p class="card-category">Completed Tasks</p>
+                                    <h4 class="card-title">{{ $tasks }}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card card-stats card-round">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-icon">
+                                <div class="icon-big text-center icon-primary bubble-shadow-small">
+                                    <i class="fas fa-users"></i>
+                                </div>
+                            </div>
+                            <div class="col col-stats ms-3">
+                                <div class="numbers">
+                                    <p class="card-category">Completed Tasks</p>
+                                    <h4 class="card-title">{{ $tasks }}</h4>
+>>>>>>> Stashed changes
                                 </div>
                             </div>
                         </div>
