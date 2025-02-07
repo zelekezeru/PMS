7ad1ffa2553052bff63d21b24c95f4016b77d428
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-02-07 03:24:20 +0300

    seeder

--- database/seeders/StrategySeeder.php
+++ database/seeders/StrategySeeder.php
@@ -17,7 +17,7 @@ class StrategySeeder extends Seeder
             ['name' => '1. Make SITS programs accessible to potential  and existing  students at any time and place!','pillar_name' => 'Pillars 1: Program and Content Delivery Mode', 'description' => 'Description for Strategy 1'],
             ['name' => '2. Realize exponential growth of students body at SITS in all programs.','pillar_name' => 'Pillar 2: Students and Students’ Enrollment', 'description' => 'Description for Strategy 2'],
             ['name' => '3.Create viable and regular income source for SITS through rendering services to the general public!','pillar_name' => 'Pillar 3: Finance',  'description' => 'Description for Strategy 3'],
-            ['name' => '4. Acquire and retain adequate qualified personnel and up-to-date technology and key facilities.','pillar_name' => 'Pillar 3: Finance',  'description' => 'Description for Strategy 4'],
+            ['name' => '4. Acquire and retain adequate qualified personnel and up-to-date technology and key facilities.','pillar_name' => 'Pillar 4: Organizational Capacity- human resources, technology and facilit',  'description' => 'Description for Strategy 4'],
             ['name' => '5. Develop an effective leadership development process in all SITS position','pillar_name' => 'Pillar 5: Governance, Leadership Development and Partnership',  'description' => 'Description for Strategy 5'],
             ]);
     }
