82d10ada0adffa43d52419cc427e8d30858a6ada
Abel Shiferaw <abelshiferaw69@gmail.com>
2025-02-14 00:52:35 +0300

    made the style.css files mutable for

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,10 @@
 
     <link rel="stylesheet" href="{{ asset('css/kaiadmin.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/fonts.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}" /> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
 
+    @vite(['resources/css/style.css'])
 
 </head>
 
