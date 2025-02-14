db7be8af81e0651c185933bf275e0e665542aa70
Eyasu Mesele <106749746+12joshua@users.noreply.github.com>
2025-02-14 01:02:30 -0800

    Update header.blade.php
    for mail logo and name

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ route('login') }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === config('app.name'))
 <img src="{{asset('img/logo.png')}}" class="logo" alt="sits Logo">
 @else
 {{ $slot }}
