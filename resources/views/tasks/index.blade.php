e46a7cd916aac16bc430176af5d6f767fcf21d8a
Abel Shiferaw <109241143+ShiferawAbel@users.noreply.github.com>
2025-03-21 02:06:02 +0300

    modified the ui to make things more clear
    - I will leave more modifications to Eyasu

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,7 +10,23 @@
                         ? 'Current Fortnight Tasks' 
                         : (request('onlyToday') ? 'Tasks For Today' : 'All Tasks List'))
                 }}
-                
+                                <div class="row d-grid gap-2 d-md-flex justify-content-md-end mb-3">
+
+                                    <div class="col-2 btn-group dropdown">
+                                        <button
+                                          class="btn btn-primary dropdown-toggle"
+                                          type="button"
+                                          data-bs-toggle="dropdown">
+                                          Create Task
+                                        </button>
+                                        <ul class="dropdown-menu" role="menu">
+                                          <li>
+                                            <a class="dropdown-item" href="{{ route('tasks.create', ['dailyTask' => true]) }}"><i class="fa fa-plus"></i> Create Daily Task</a>
+                                            <a class="dropdown-item" href="{{ route('tasks.create') }}"><i class="fa fa-plus"></i> Create Fortnight Task</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
             </h2>
             
             @if (request('currentFortnight') )
@@ -32,59 +48,47 @@
                 <div class="col text-center">
                     <h5 class="nav-item text-info justify-content-md-start "> All Registered Tasks</h5>
                 </div>
-
+                
             @endif
-            
             <div class="card-body">
-                <div class="row d-grid gap-2 d-md-flex justify-content-md-end mb-3">
 
-                    <div class="col-2 btn-group dropdown">
-                        <button
-                          class="btn btn-primary dropdown-toggle"
-                          type="button"
-                          data-bs-toggle="dropdown">
-                          Create Task
-                        </button>
-                        <ul class="dropdown-menu" role="menu">
-                          <li>
-                            <a class="dropdown-item" href="{{ route('tasks.create', ['dailyTask' => true]) }}"><i class="fa fa-plus"></i> Create Daily Task</a>
-                            <a class="dropdown-item" href="{{ route('tasks.create') }}"><i class="fa fa-plus"></i> Create Fortnight Task</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <ul class="nav nav-tabs nav-line nav-color-secondary" id="line-tab" role="tablist">
-                    @if (request()->user()->hasAnyRole(['SUPER_ADMIN','ADMIN', 'DEPARTMENT_HEAD']))
-                        @hasrole('DEPARTMENT_HEAD')
+
+                <div class="d-flex justify-content-center">
+                    <ul class="nav nav-tabs nav-line nav-color-secondary text-center" id="line-tab" role="tablist">
+                        @if (request()->user()->hasAnyRole(['SUPER_ADMIN','ADMIN', 'DEPARTMENT_HEAD']))
+                            @hasrole('DEPARTMENT_HEAD')
+                                <li class="nav-item">
+                                    <a class="nav-link btn {{ request('currentFortnight') || request('onlyToday') || request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">All Department Tasks</a>
+                                </li>
+                            @else
+                                <li class="nav-item">
+                                    <a class="nav-link btn {{ request('currentFortnight') || request('onlyToday') || request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">All Tasks</a>
+                                </li>
+                            @endhasrole
+
                             <li class="nav-item">
-                                <a class="nav-link btn {{ request('currentFortnight') || request('onlyToday') || request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">All Department Tasks</a>
+                                <a class="nav-link btn {{ request('myTasks') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['myTasks' => true])}}" role="tab" aria-controls="pills-home" aria-selected="true">My Tasks</a>
                             </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link btn {{ request('currentFortnight') || request('onlyToday') || request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">All Tasks</a>
-                            </li>
-
-                        @endhasrole
-
-                        <li class="nav-item">
-                            <a class="nav-link btn {{ request('myTasks') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['myTasks' => true])}}" role="tab" aria-controls="pills-home" aria-selected="true">My Tasks</a>
-                        </li>
-                    
-                    @elseif(request()->user()->hasRole('SUPER-ADMIN'))
-                        <li class="nav-item">
-                            <a class="nav-link btn {{ request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">My Tasks</a>
-                        </li>
                         
-                    @endif
+                        @elseif(request()->user()->hasRole('SUPER-ADMIN'))
+                            <li class="nav-item">
+                                <a class="nav-link btn {{ request('myTasks') ? '' : 'active' }}" id="line-home-tab" href="{{route('tasks.index')}}" role="tab" aria-controls="pills-home" aria-selected="true">My Tasks</a>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
 
-                    <li class="nav-item">
-                        <a class="nav-link btn {{ request('currentFortnight') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['currentFortnight' => true, 'myTasks' => request('myTasks')])}}" role="tab" aria-controls="pills-home" aria-selected="true">Current Fortnight</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn {{ request('onlyToday') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['onlyToday' => true, 'myTasks' => request('myTasks')])}}" role="tab" aria-controls="pills-home" aria-selected="true">Today's Tasks</a>
-                    </li>
-                    
-                </ul>
+                <div class="d-flex justify-content-center mt-1">
+                    <ul class="nav nav-tabs nav-line nav-color-secondary" id="line-tab" role="tablist">
+                        <li class="nav-item">
+                            <a class="nav-link btn {{ request('currentFortnight') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['currentFortnight' => true, 'myTasks' => request('myTasks')])}}" role="tab" aria-controls="pills-home" aria-selected="true"> {{ request('myTasks') ? 'My' : 'All' }} Tasks Of Current Fortnight</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link btn {{ request('onlyToday') ? 'active' : '' }}" id="line-home-tab" href="{{route('tasks.index', ['onlyToday' => true, 'myTasks' => request('myTasks')])}}" role="tab" aria-controls="pills-home" aria-selected="true">{{ request('myTasks') ? 'My' : 'All' }} Task's For Today</a>
+                        </li>
+                    </ul>
+                </div>
+
                 
                 @include('tasks.list')
 
