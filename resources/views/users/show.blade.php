61f27088f67ef7b5c3f49f118ce3048f9e1fa77f
Abi Zeleke <57219079+zelekezeru@users.noreply.github.com>
2025-12-23 18:43:52 +0300

    Update show.blade.php

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -83,7 +83,6 @@
                         @endif
                     </tr>
                 @endif
-                {{$user}}
                 @if (request()->user()->hasAnyRole(['SUPER_ADMIN', 'ADMIN']))
 
                     @if($user->default_password)
@@ -144,7 +143,7 @@
 <div class="row mt-4">
     <!-- Today's Performance -->
     <div class="col-12 col-md-6 col-lg-8 mb-6">
-        
+
         {{-- List daily tasks --}}
         <ul class="list-group">
             <li class="list-group-item active d-flex justify-content-between align-items-center">
